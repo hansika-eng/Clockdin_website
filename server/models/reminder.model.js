1d1ed56d779eccd61de45e18eee3da1ae1fefa34
Hansika Indukuri <hemasaihansika@gmail.com>
2026-05-14 16:29:10 +0530

    fixed reminder schema

--- server/models/reminder.model.js
+++ server/models/reminder.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const reminderSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 
-  event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+  event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' },
 
   email: { type: String, required: true },
 
